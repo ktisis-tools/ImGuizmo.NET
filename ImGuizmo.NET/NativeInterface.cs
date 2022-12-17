c1116531c77abf95eacd10ba10e720c9dcf6b3d3
Fayti1703 <fayti1703@protonmail.com>
2022-12-17 12:24:13 +0100

    [C#] Add missing `DllImportAttribute`s

--- ImGuizmo.NET/NativeInterface.cs
+++ ImGuizmo.NET/NativeInterface.cs
@@ -9,18 +9,27 @@ internal static class NativeInterface {
 	[DllImport("ImGuizmo-Bridge")]
 	extern internal static void Ktisis_ImGuizmo_SetAllocatorFunctions(IntPtr alloc, IntPtr free, IntPtr ud);
 
+	[DllImport("ImGuizmo-Bridge")]
 	extern internal static unsafe void Ktisis_ImGuizmo_DecomposeMatrixToComponents(float* matrix, float* translation, float* rotation, float* scale);
+	[DllImport("ImGuizmo-Bridge")]
 	extern internal static unsafe void Ktisis_ImGuizmo_RecomposeMatrixFromComponents(float* translation, float* rotation, float* scale, float* matrix);
 
+	[DllImport("ImGuizmo-Bridge")]
 	extern internal static unsafe Style* Ktisis_ImGuizmo_GetStyle();
 
+	[DllImport("ImGuizmo-Bridge")]
 	extern internal static void Ktisis_ImGuizmo_AllowAxisFlip(bool value);
 
+	[DllImport("ImGuizmo-Bridge")]
 	extern internal static void Ktisis_ImGuizmo_BeginFrame();
+	[DllImport("ImGuizmo-Bridge")]
 	extern internal static void Ktisis_ImGuizmo_SetDrawlist(IntPtr drawList);
+	[DllImport("ImGuizmo-Bridge")]
 	extern internal static void Ktisis_ImGuizmo_SetRect(float x, float y, float width, float height);
+	[DllImport("ImGuizmo-Bridge")]
 	extern internal static void Ktisis_ImGuizmo_Enable(bool enabled);
 
+	[DllImport("ImGuizmo-Bridge")]
 	extern internal static unsafe bool Ktisis_ImGuizmo_Manipulate(
 		float* view,
 		float* projection,
@@ -33,7 +42,9 @@ internal static class NativeInterface {
 		float* boundsSnap
 	);
 
+	[DllImport("ImGuizmo-Bridge")]
 	extern internal static bool Ktisis_ImGuizmo_IsUsing();
+	[DllImport("ImGuizmo-Bridge")]
 	extern internal static bool Ktisis_ImGuizmo_IsOver0();
 
 }
